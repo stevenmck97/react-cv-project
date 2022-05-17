c0249bebf12b5c8e7d7e8754d18d02b3832af9c1
Steven McKeown <stevenmckeown19@gmail.com>
2022-05-17 17:50:48 +0100

    separated jsx into more divs

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -32,55 +32,73 @@ class App extends Component {
         return (
             <div>
                 <form>
-                    <h1>Personal Details</h1>
-                    <label htmlFor="genName">
-                        Name:
-                        <input type="text" value={name} id="genName" />
-                    </label>
-                    <label htmlFor="genEmail">
-                        Email:
-                        <input type="email" value={email} id="genEmail" />
-                    </label>
-                    <label htmlFor="genPhone">
-                        Phone Number:
-                        <input type="number" value={phoneNo} id="genPhone" />
-                    </label>
+                    <div>
+                        <h1>Personal Details</h1>
+                        <label htmlFor="genName">
+                            Name:
+                            <input type="text" value={name} id="genName" />
+                        </label>
+                        <label htmlFor="genEmail">
+                            Email:
+                            <input type="email" value={email} id="genEmail" />
+                        </label>
+                        <label htmlFor="genPhone">
+                            Phone Number:
+                            <input
+                                type="number"
+                                value={phoneNo}
+                                id="genPhone"
+                            />
+                        </label>
+                    </div>
 
-                    <h1>Education</h1>
-                    <label htmlFor="school">
-                        School Name:
-                        <input type="text" value={school} id="school" />
-                    </label>
-                    <label htmlFor="course">
-                        Course Name:
-                        <input type="text" value={course} id="course" />
-                    </label>
-                    <label htmlFor="edDuration">
-                        Duration:
-                        <input type="text" value={edDuration} id="edDuration" />
-                    </label>
-                    <h1>Work Experience</h1>
-                    <label htmlFor="company">
-                        Company:
-                        <input type="text" value={company} id="company" />
-                    </label>
-                    <label htmlFor="position">
-                        Position:
-                        <input type="text" value={position} id="position" />
-                    </label>
-                    <label htmlFor="tasks">
-                        Tasks:
-                        <input type="text" value={tasks} id="tasks" />
-                    </label>
-                    <label htmlFor="workDuration">
-                        Duration:
-                        <input
-                            type="text"
-                            value={workDuration}
-                            id="workDuration"
-                        />
-                    </label>
-                    <button type="submit">Create CV</button>
+                    <div>
+                        <h1>Education</h1>
+                        <label htmlFor="school">
+                            School Name:
+                            <input type="text" value={school} id="school" />
+                        </label>
+                        <label htmlFor="course">
+                            Course Name:
+                            <input type="text" value={course} id="course" />
+                        </label>
+                        <label htmlFor="edDuration">
+                            Duration:
+                            <input
+                                type="text"
+                                value={edDuration}
+                                id="edDuration"
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <h1>Work Experience</h1>
+                        <label htmlFor="company">
+                            Company:
+                            <input type="text" value={company} id="company" />
+                        </label>
+                        <label htmlFor="position">
+                            Position:
+                            <input type="text" value={position} id="position" />
+                        </label>
+                        <label htmlFor="tasks">
+                            Tasks:
+                            <input type="text" value={tasks} id="tasks" />
+                        </label>
+                        <label htmlFor="workDuration">
+                            Duration:
+                            <input
+                                type="text"
+                                value={workDuration}
+                                id="workDuration"
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <button type="submit">Create CV</button>
+                    </div>
                 </form>
             </div>
         );
