a70c3e9335ccd6c50f60ce436f582543ead48044
Steven McKeown <stevenmckeown19@gmail.com>
2022-05-17 17:48:06 +0100

    implemented destructuring to improve readability

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -13,102 +13,70 @@ class App extends Component {
             education: {
                 school: "",
                 course: "",
-                duration: "",
+                edDuration: "",
             },
             work: {
                 company: "",
                 position: "",
                 tasks: "",
-                duration: "",
+                workDuration: "",
             },
         };
     }
 
     render() {
+        const { name, email, phoneNo } = this.state.general;
+        const { school, course, edDuration } = this.state.education;
+        const { company, position, tasks, workDuration } = this.state.work;
+
         return (
             <div>
                 <form>
                     <h1>Personal Details</h1>
                     <label htmlFor="genName">
                         Name:
-                        <input
-                            type="text"
-                            value={this.state.general.name}
-                            id="genName"
-                        />
+                        <input type="text" value={name} id="genName" />
                     </label>
                     <label htmlFor="genEmail">
                         Email:
-                        <input
-                            type="email"
-                            value={this.state.general.email}
-                            id="genEmail"
-                        />
+                        <input type="email" value={email} id="genEmail" />
                     </label>
                     <label htmlFor="genPhone">
                         Phone Number:
-                        <input
-                            type="number"
-                            value={this.state.general.phoneNo}
-                            id="genPhone"
-                        />
+                        <input type="number" value={phoneNo} id="genPhone" />
                     </label>
 
                     <h1>Education</h1>
                     <label htmlFor="school">
                         School Name:
-                        <input
-                            type="text"
-                            value={this.state.education.school}
-                            id="school"
-                        />
+                        <input type="text" value={school} id="school" />
                     </label>
                     <label htmlFor="course">
                         Course Name:
-                        <input
-                            type="text"
-                            value={this.state.education.course}
-                            id="course"
-                        />
+                        <input type="text" value={course} id="course" />
                     </label>
-                    <label htmlFor="duration">
+                    <label htmlFor="edDuration">
                         Duration:
-                        <input
-                            type="text"
-                            value={this.state.education.duration}
-                            id="duration"
-                        />
+                        <input type="text" value={edDuration} id="edDuration" />
                     </label>
                     <h1>Work Experience</h1>
                     <label htmlFor="company">
                         Company:
-                        <input
-                            type="text"
-                            value={this.state.work.company}
-                            id="company"
-                        />
+                        <input type="text" value={company} id="company" />
                     </label>
                     <label htmlFor="position">
                         Position:
-                        <input
-                            type="text"
-                            value={this.state.work.position}
-                            id="position"
-                        />
+                        <input type="text" value={position} id="position" />
                     </label>
                     <label htmlFor="tasks">
                         Tasks:
-                        <input
-                            type="text"
-                            value={this.state.work.tasks}
-                            id="tasks"
-                        />
+                        <input type="text" value={tasks} id="tasks" />
                     </label>
                     <label htmlFor="workDuration">
                         Duration:
                         <input
                             type="text"
-                            value={this.state.work.duration}
+                            value={workDuration}
                             id="workDuration"
                         />
                     </label>
