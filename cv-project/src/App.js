94bdefd66c6f4367181cba9bd753458143d9b47c
Steven McKeown <stevenmckeown19@gmail.com>
2022-05-23 17:46:35 +0100

    Refactored App from class component to functional component and implemented useState hook.

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,82 +1,61 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Education from "./components/Education";
 import General from "./components/General";
 import Work from "./components/Work";
 
-class App extends Component {
-    constructor() {
-        super();
+const App = () => {
+    const [cv, setCV] = useState({
+        name: "",
+        email: "",
+        phoneNo: "",
 
-        this.state = {
-            name: "",
-            email: "",
-            phoneNo: "",
+        school: "",
+        course: "",
+        edDuration: "",
 
-            school: "",
-            course: "",
-            edDuration: "",
+        company: "",
+        position: "",
+        tasks: "",
+        workDuration: "",
+    });
 
-            company: "",
-            position: "",
-            tasks: "",
-            workDuration: "",
-        };
-    }
-
-    handleInputChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        });
+    const handleInputChange = (e) => {
+        setCV({ ...cv, [e.target.name]: e.target.value });
     };
 
-    render() {
-        const {
-            name,
-            email,
-            phoneNo,
-            school,
-            course,
-            edDuration,
-            company,
-            position,
-            tasks,
-            workDuration,
-        } = this.state;
+    return (
+        <div>
+            <form>
+                <General
+                    firstName={cv.name}
+                    email={cv.email}
+                    phoneNo={cv.phoneNo}
+                    onChange={handleInputChange}
+                />
 
-        return (
+                <Education
+                    school={cv.school}
+                    course={cv.course}
+                    edDuration={cv.edDuration}
+                    onChange={handleInputChange}
+                />
+
+                <Work
+                    company={cv.company}
+                    position={cv.position}
+                    tasks={cv.tasks}
+                    workDuration={cv.workDuration}
+                    onChange={handleInputChange}
+                />
+
+                <button type="submit">Create CV</button>
+            </form>
             <div>
-                <form>
-                    <General
-                        firstName={name}
-                        email={email}
-                        phoneNo={phoneNo}
-                        onChange={this.handleInputChange}
-                    />
-
-                    <Education
-                        school={school}
-                        course={course}
-                        edDuration={edDuration}
-                        onChange={this.handleInputChange}
-                    />
-
-                    <Work
-                        company={company}
-                        position={position}
-                        tasks={tasks}
-                        workDuration={workDuration}
-                        onChange={this.handleInputChange}
-                    />
-
-                    <button type="submit">Create CV</button>
-                </form>
-                <div>
-                    <li>{name}</li>
-                    <li>{email}</li>
-                </div>
+                <li>{cv.name}</li>
+                <li>{cv.email}</li>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
